9974f0e5ef20ecd8bfe0b038b41e3f378f5c983a
Andres Zurita <Fehu.developers@gmail.com>
2025-12-31 11:28:13 -0300

    Agregar logging

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -147,3 +147,24 @@ EMAIL_HOST_PASSWORD = 'incsnjaojecdypzb'  # Usar App Password de Google
 DEFAULT_FROM_EMAIL = 'Fehu Developers <fehu.developers@gmail.com>'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
